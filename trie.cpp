8ae6f03587d0cc9c81dccfe52762265b00cc3356
Shiki93 <murillo.tec@gmail.com>
2016-06-07 01:34:09 -0600

    Add files via upload

--- trie.cpp
+++ trie.cpp
@@ -12,3 +12,16 @@ void Trie::insert(std::string s){
     }
     curr->setIsEnd(true);
 }
+
+bool Trie::search(std::string s){
+    Node *curr = root;
+    curr->incPrefixCount();
+    for(int i = 0; i < s.length(); i++){
+        if(curr->findChild(s[i])){
+            return true;
+        }
+        curr = curr->findChild(s[i]);
+        curr->incPrefixCount();
+    }
+    curr->setIsEnd(true);
+}
