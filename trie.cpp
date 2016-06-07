44a389b70b06b8834ab293b6a73744c807874820
andmadrigal <andresgarro18@gmail.com>
2016-06-07 01:45:49 -0600

    Add files via upload

--- trie.cpp
+++ trie.cpp
@@ -25,3 +25,34 @@ bool Trie::search(std::string s){
     }
     curr->setIsEnd(true);
 }
+
+void Trie::search(struct trie* root, string str){
+        struct trie* cur = root;
+        for (int i=0; i< str.length() ; i++)
+        {
+            int index = CHAR_TO_INDEX(str[i]);
+            if(!cur->children[index])
+            {
+                cout<<str<<" does not Exist\n";
+                return;
+            }
+            cur=cur->children[index];
+        }
+        cout<<str<<" Exists\n";
+        return;
+}
+
+void Trie::insert(struct trie* root, string str){
+        struct trie* cur = root;
+        for (int i=0; i< str.length(); i++)
+        {
+            int index = CHAR_TO_INDEX(str[i]);
+            if(!cur->children[index])
+                cur->children[index] = getNode();
+            cur=cur->children[index];
+        }
+        cur->isLeaf = true;
+        return;
+}
+
+
