57af799459cb660ba4ccdac7a4b51c70076dc113
Shiki93 <murillo.tec@gmail.com>
2016-06-07 01:15:42 -0600

    Add files via upload

--- trie.cpp
+++ trie.cpp
@@ -1,5 +1,14 @@
 #include "trie.h"
 
-Trie::Trie()
-{
+void Trie::insert(std::string s){
+    Node *curr = root;
+    curr->incPrefixCount();
+    for(int i = 0; i < s.length(); i++){
+        if(curr->findChild(s[i]) == NULL){
+            curr->appendChild(s[i]);
+        }
+        curr = curr->findChild(s[i]);
+        curr->incPrefixCount();        
+    }
+    curr->setIsEnd(true);
 }
--- trie.h
+++ trie.h
@@ -1,10 +1,32 @@
 #ifndef TRIE_H
 #define TRIE_H
+#include <QtCore>
 
-class Trie
-{
+class Node{
+private:
+    bool isEnd;
+    int prefixCount;
+    Node *child[ALPHABET_SIZE];
+    
 public:
-    Trie();
+    Node(){prefixCount = 0; isEnd = false;}
+    bool getIsEnd(){return isEnd;}
+    void setIsEnd(bool i){isEnd = i;}
+    int getPrefixCount(){return prefixCount;}
+    void incPrefixCount(){prefixCount++;}
+    void decPrefixCount(){prefixCount--;}
+    Node *findChild(char c);
+    void appendChild(char c);
+};
+
+class Trie{
+public:
+    Node *root;
+    Trie(){root = new Node();}
+    void insert(std::string s);
+    bool search(std::string s);
+    void delet(std::string s);
+    int wordWithPrefix(std::string prefix);
 };
 
 #endif // TRIE_H
